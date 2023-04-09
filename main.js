618e789ebfab85a5f5430a33f6beb624e6cd8d9d
rolsorstart <104572207+rolsorstart@users.noreply.github.com>
2023-04-09 12:40:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,4 +13,21 @@ recognition.onresult=function(event){
     var Content=event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML=Content;
+    speak();
 }
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");

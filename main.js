a2b18d26b1c5041803c88a3d6a1bb41e0eae8bdf
rolsorstart <104572207+rolsorstart@users.noreply.github.com>
2023-04-16 12:47:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,14 +13,23 @@ recognition.onresult=function(event){
     var Content=event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML=Content;
-    speak();
+    if(Content =="Take my selfie."){
+        console.log("taking selfie --- ");
+    
+    speak();}
 }
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data="Taking Your Selfie In 5 Seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+        }    , 5000);
+    
 }
 
 
@@ -31,3 +40,15 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">'
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
